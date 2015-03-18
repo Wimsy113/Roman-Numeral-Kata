17a57195ab349d470febf4c99da06919cad6cd64
Krystyna Ewing <krystyna.d.l.ewing@gmail.com>
2015-03-18 16:47:32 -0400

    Solution started

---
+++ Solution.rb
@@ -0,0 +1,19 @@
+def to_roman(number)
+	numeral = {
+		1 => "I"
+		5 => "V"
+		10 => "X"
+		50 => "L"
+		
+}
+
+roman_string = ""
+
+numeral.each do |arabic, roman|
+	if number % arabic < num
+		number -= arabic
+		roman_string += roman_string
+	end
+end
+roman_string
+end
